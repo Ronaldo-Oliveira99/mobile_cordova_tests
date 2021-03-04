7fce6f00fc540024122a90efa00d6bf8621be92b
Ronaldo de Oliveira Luiz <ronaldoluiz747@gmail.com>
2021-03-04 17:59:49 -0300

    update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -24,6 +24,45 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
+    document.querySelector('#loadPDF1').addEventListener('touchend', loadPDF1,false);
+    document.querySelector('#loadPDF2').addEventListener('touchend', loadPDF2,false);
+    document.querySelector('#loadPDF3').addEventListener('touchend', loadPDF3,false);
+
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+}
+
+function loadPDF1() {
+  console.log('loadPDF1');
+  document.location.href='docs/documento.pdf';
+}
+
+function loadPDF2() {
+  console.log('loadPDF2');
+  var ref = cordova.InAppBrowser.open('docs/documento.pdf', '_blank', 'location=no');
+}
+
+function loadPDF3() {
+  console.log('loadPDF3');
+  console.log(cordova.file.applicationDirectory);
+  window.resolveLocalFileSystemURL(cordova.file.applicationDirectory +  'www/docs/documento.pdf', function(fileEntry) {
+      
+      window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dirEntry) {
+          
+          fileEntry.copyTo(dirEntry, 'file.pdf', function(newFileEntry) {
+
+
+              cordova.plugins.fileOpener2.open(newFileEntry.nativeURL,'application/pdf',
+              { 
+                  error : function(e) { 
+                      console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+                  },
+                  success : function () {
+                      console.log('file opened successfully'); 				
+                  }
+              }
+              );
+          });
+      });
+  });
 }
